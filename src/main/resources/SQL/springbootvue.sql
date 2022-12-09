830b99a66da998759312827e7db4170d1febdc33
xie <xie@qq.com>
2022-12-09 10:46:59 +0800

    版本1.0-补充数据库文件springbootvue.sql

---
+++ src/main/resources/SQL/springbootvue.sql
@@ -0,0 +1,172 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql
+ Source Server Type    : MySQL
+ Source Server Version : 80018
+ Source Host           : localhost:3306
+ Source Schema         : springbootvue
+
+ Target Server Type    : MySQL
+ Target Server Version : 80018
+ File Encoding         : 65001
+
+ Date: 09/12/2022 10:44:28
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tb_admin
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_admin`;
+CREATE TABLE `tb_admin`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ano` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '管理员账号',
+  `name` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '姓名',
+  `gender` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '性别',
+  `password` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `telephone` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '电话',
+  `address` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '地址',
+  `portrait_path` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '头像',
+  `isdelete` bit(1) NULL DEFAULT NULL COMMENT '账号是否禁用',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 158 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_admin
+-- ----------------------------
+INSERT INTO `tb_admin` VALUES (155, '456', '黄宇辉', '男', '1234com', '3083968068@qq.com', '15111111111', '河南周口', 'image/admin.gif', b'0');
+INSERT INTO `tb_admin` VALUES (156, '123', '梁爽', '男', '1234com', '123@qq.com', '1', '1', 'mage/admin.gif', b'1');
+INSERT INTO `tb_admin` VALUES (157, '789', '冷风', '女', '123com', NULL, NULL, NULL, NULL, b'1');
+
+-- ----------------------------
+-- Table structure for tb_class
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_class`;
+CREATE TABLE `tb_class`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tno` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '教师编号',
+  `name` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '教师名',
+  `cno` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '任课班级编号',
+  `clazz_name` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '班级名称',
+  `content` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '任课内容',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_class
+-- ----------------------------
+INSERT INTO `tb_class` VALUES (2, '389432', '黄亮', '702', '七年级二班', NULL);
+INSERT INTO `tb_class` VALUES (3, '389432', '黄亮', '123', '六年级一班', '语文');
+INSERT INTO `tb_class` VALUES (4, '345678', '刘风', '123', '六年级一班', NULL);
+INSERT INTO `tb_class` VALUES (5, '345678', '刘风', '702', '七年级二班', '数学');
+
+-- ----------------------------
+-- Table structure for tb_clazz
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_clazz`;
+CREATE TABLE `tb_clazz`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cno` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '班级编号',
+  `name` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '班级名称',
+  `number` int(3) NOT NULL COMMENT '班级总人数',
+  `introducation` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '班级简介',
+  `coordinator` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '班级任',
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '班主任邮箱',
+  `telephone` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '班主任邮箱',
+  `gno` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '所属年级编号',
+  `grade_name` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '所属年级名称',
+  `isdelete` bit(1) NULL DEFAULT NULL COMMENT '是否已删除（用状态记录数据是否被删除）',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `tb_clazz_tb_grade__fk_idx`(`grade_name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 70 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_clazz
+-- ----------------------------
+INSERT INTO `tb_clazz` VALUES (68, '123', '六年级一班', 0, '六年级一班', '谢军', '123@qq.com', '13345678988', NULL, '六年级', NULL);
+INSERT INTO `tb_clazz` VALUES (69, '702', '七年级二班', 1, '', '刘风', '123sd@qq.com', '78945158', '700', '七年级', b'0');
+
+-- ----------------------------
+-- Table structure for tb_grade
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_grade`;
+CREATE TABLE `tb_grade`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gno` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '年级编号',
+  `name` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '年级名称',
+  `manager` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '年级主任',
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '主任邮箱',
+  `telephone` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '主任电话',
+  `introducation` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '年级介绍',
+  `isdelete` bit(1) NULL DEFAULT NULL COMMENT '是否已删除（用状态记录数据是否被删除）',
+  PRIMARY KEY (`id`, `gno`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 97 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_grade
+-- ----------------------------
+INSERT INTO `tb_grade` VALUES (93, '234', '六年级', '钟文涛', '123@qq.com', '15818916687', '六年级', NULL);
+INSERT INTO `tb_grade` VALUES (94, '235', '五年级', '徐文', '22@qq.com', '15818916687', '1', b'0');
+INSERT INTO `tb_grade` VALUES (95, '122', 'scz', '黄亮', '123@qq.com', '15818916687', '', b'1');
+INSERT INTO `tb_grade` VALUES (96, '700', '七年级', '刘风', '123sd@qq.com', '78945158', '', b'0');
+
+-- ----------------------------
+-- Table structure for tb_student
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_student`;
+CREATE TABLE `tb_student`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sno` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '学生账号',
+  `name` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '姓名',
+  `gender` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '性别',
+  `password` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `telephone` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '电话',
+  `address` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '地址',
+  `introducation` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '个人简介',
+  `portrait_path` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '头像',
+  `clazz_name` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '所属班级名称',
+  `cno` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '所属班级编号',
+  `isdelete` bit(1) NULL DEFAULT NULL COMMENT '账号是否禁用',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `tb_student_sno_uindex`(`sno`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_student
+-- ----------------------------
+INSERT INTO `tb_student` VALUES (7, '396012', '谢良', '男', '123.com', '1111@qq.com', '15818915555', '广东', '你好', 'image/pika.gif', NULL, NULL, b'0');
+INSERT INTO `tb_student` VALUES (8, '389555', '吴天', '男', 'afdsgfdgdtf', NULL, NULL, NULL, NULL, NULL, '七年级二班', '702', b'0');
+
+-- ----------------------------
+-- Table structure for tb_teacher
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_teacher`;
+CREATE TABLE `tb_teacher`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tno` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '教师账号',
+  `name` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '姓名',
+  `gender` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '性别',
+  `password` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `telephone` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '电话',
+  `address` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '地址',
+  `portrait_path` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '头像',
+  `clazz_name` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '任课班级（暂不使用）',
+  `cno` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '任课班级变化（暂不使用）',
+  `isdelete` bit(1) NULL DEFAULT NULL COMMENT '账号是否禁用',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `tb_teacher_tno_uindex`(`tno`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_teacher
+-- ----------------------------
+INSERT INTO `tb_teacher` VALUES (12, '389432', '黄亮', '男', '123456', '123@qq.com', '15818916687', '广东', 'image/run.gif', '六年级一班', NULL, b'0');
+INSERT INTO `tb_teacher` VALUES (13, '345678', '刘风', '男', '1234567', '123sd@qq.com', '78945158', '北京朝阳', NULL, NULL, NULL, b'0');
+
+SET FOREIGN_KEY_CHECKS = 1;
